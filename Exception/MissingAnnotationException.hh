4057d574260edd3941084074cfbdea8d0ae923f7
Miles Johnson <miles.johnson@hyfn.com>
2015-02-09 00:29:45 -0800

    Separated annotation functionality into its own package. [#51]

---
+++ Exception/MissingAnnotationException.hh
@@ -0,0 +1,17 @@
+<?hh // strict
+/**
+ * @copyright   2010-2015, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Annotation\Exception;
+
+/**
+ * Exception thrown when an annotation by name does not exist.
+ *
+ * @package Titon\Annotation\Exception
+ */
+class MissingAnnotationException extends \OutOfBoundsException {
+
+}
